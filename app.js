0844be0d33d8786feaf0be4c7a6be0078d01ecc2
dydkask <dydkask@gmail.com>
2018-10-07 14:52:21 +0900

    serverless

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send({
+    Output: 'Hello World! commit test',
+  });
+});
+
+app.get('/foo', function(req, res) {
+  res.send({
+    Output: 'Hello World!gettest',
+  });
+});
+
+app.post('/', function(req, res) {
+  res.send({
+    Output: 'Hello World!',
+  });
+});
+
+// Export your Express configuration so that it can be consumed by the Lambda handler
+module.exports = app;
