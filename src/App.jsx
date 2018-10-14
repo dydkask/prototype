9d8e64feaac31110aca6fe4e9567fd5828b1a689
devJang <devjang2016@gmail.com>
2018-10-14 14:48:02 +0900

    Update: HTML template

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,39 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {
+          id: 0,
+          text: 'hello',
+          completed: true,
+        },
+        {
+          id: 1,
+          text: 'bye',
+          completed: false,
+        },
+      ],
+    };
+  }
+
+  handleInput(event) {
+    if (!event.keyCode === 13) return;
+    console.log(event.keyCode);
+    console.log(event.target.value);
+  }
+
   render() {
+    const { todos } = this.state;
+
     return (
       <section className="todoapp">
-        <Header />
-        <Main />
-        <Footer />
+        <Header todos={todos} handleInput={this.handleInput} />
+        <Main todos={todos} />
+        <Footer todos={todos} />
       </section>
     );
   }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 export default class Item extends Component {
   render() {
+    const { text, completed } = this.props.todo;
+
     return (
       <li>
         <div className="view">
           <input className="toggle" type="checkbox" />
-          <label />
+          <label>{text}</label>
           <button className="destroy" />
         </div>
         <input className="edit" />
